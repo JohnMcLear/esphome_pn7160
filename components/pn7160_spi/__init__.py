f8f6cd18afd8ae7198ceec6a666026dc1249d950
John McLear <john@mclear.co.uk>
2026-02-25 13:38:46 +0000

    spi fix

--- components/pn7160_spi/__init__.py
+++ components/pn7160_spi/__init__.py
@@ -1,10 +1,9 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import spi
-from esphome.const import CONF_ID, CONF_CS_PIN
+from esphome.const import CONF_ID
 
 from .. import pn7160
-from esphome import pins
 
 DEPENDENCIES = ["spi"]
 AUTO_LOAD = ["pn7160"]
@@ -16,7 +15,6 @@ CONFIG_SCHEMA = (
     pn7160.PN7160_SCHEMA.extend(
         {
             cv.GenerateID(): cv.declare_id(PN7160Spi),
-            cv.Required(CONF_CS_PIN): pins.gpio_output_pin_schema,
         }
     )
     .extend(spi.spi_device_schema())
@@ -26,9 +24,5 @@ CONFIG_SCHEMA = (
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await spi.register_spi_device(var, config)
-
-    cs_pin = await cg.gpio_pin_expression(config[CONF_CS_PIN])
-    cg.add(var.set_cs_pin(cs_pin))
-
     await pn7160.setup_pn7160(var, config)
 
